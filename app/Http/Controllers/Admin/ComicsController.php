41dfeee0d198c83d832d4216e0e3bf3c720bdb2f
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-07-26 18:42:16 +0200

    Add model Comic

--- app/Http/Controllers/Admin/ComicsController.php
+++ app/Http/Controllers/Admin/ComicsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Comic;
 
 class ComicsController extends Controller
 {
@@ -14,7 +15,7 @@ class ComicsController extends Controller
      */
     public function index()
     {
-        $comicList = Comic::all();
+        $comicList = Comic::findOrFail();
         return view('admin.comics.index', compact('comicList'));
     }
 
